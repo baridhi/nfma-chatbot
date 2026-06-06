64b418e5f5275e86a47005bfa41068a461532357
baridhi <30325396+baridhi@users.noreply.github.com>
2026-06-05 23:17:42 -0700

    Update pgpt-widget.js

--- js/pgpt-widget.js
+++ js/pgpt-widget.js
@@ -319,7 +319,7 @@
 
       <div id="pgpt-footer">
         Powered by <a href="https://github.com/baridhi/privateGPT_cfa" target="_blank">PrivateGPT</a>
-        · runs locally Baridhi's laptop
+        · runs locally on Baridhi's laptop
       </div>
     </div>
   `);
